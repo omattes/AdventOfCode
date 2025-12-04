4621f3b8c6b9cf9ed8917c5501efe0b2fedaecba
Mattes Ossenbeck <github.mattes@ossenbeck.dev>
2025-12-04 22:57:31 +0100

    Solve 2025 day 03

--- 2025/java/src/main/java/dev/ossenbeck/day03/Puzzle.java
+++ 2025/java/src/main/java/dev/ossenbeck/day03/Puzzle.java
@@ -3,18 +3,50 @@ package dev.ossenbeck.day03;
 import dev.ossenbeck.InputReader;
 import dev.ossenbeck.Solvable;
 
-public class Puzzle implements Solvable<Integer, Integer> {
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class Puzzle implements Solvable<Long, Long> {
+    private final List<int[]> banks;
+
     public Puzzle(InputReader inputReader) {
+        this.banks = inputReader.asStream()
+                .map(Puzzle::parseBatteries)
+                .toList();
+    }
 
+    private static int[] parseBatteries(String line) {
+        return line.chars().map(c -> c - '0').toArray();
     }
 
     @Override
-    public Integer solvePartOne() {
-        return 0;
+    public Long solvePartOne() {
+        return calculateTotalOutputVoltage(2);
     }
 
     @Override
-    public Integer solvePartTwo() {
-        return 0;
+    public Long solvePartTwo() {
+        return calculateTotalOutputVoltage(12);
+    }
+
+    private long calculateTotalOutputVoltage(int allowedActiveBatteries) {
+        return banks.stream()
+                .mapToLong(bank -> calculateBankOutputVoltage(bank, allowedActiveBatteries))
+                .sum();
+    }
+
+    private long calculateBankOutputVoltage(int[] bank, int allowedActiveBatteries) {
+        var voltage = 0L;
+        var activeBatteries = allowedActiveBatteries;
+        for (var i = 0; i < bank.length && --activeBatteries >= 0; i++) {
+            i = findIndexOfMaxValue(bank, i, bank.length - activeBatteries);
+            voltage = voltage * 10 + bank[i];
+        }
+        return voltage;
+    }
+
+    private int findIndexOfMaxValue(int[] arr, int start, int end) {
+        return IntStream.range(start, end)
+                .reduce(start, (max, current) -> arr[current] > arr[max] ? current : max);
     }
 }
--- 2025/java/src/test/java/dev/ossenbeck/day03/PuzzleTest.java
+++ 2025/java/src/test/java/dev/ossenbeck/day03/PuzzleTest.java
@@ -6,26 +6,26 @@ import dev.ossenbeck.SolvableTest;
 
 import java.nio.file.Path;
 
-class PuzzleTest extends SolvableTest<Integer, Integer> {
+class PuzzleTest extends SolvableTest<Long, Long> {
     private static final Path INPUT = COMMON_PATH.resolve("day03", "input.txt");
 
     @Override
-    protected Solvable<Integer, Integer> getSutPartOne() {
+    protected Solvable<Long, Long> getSutPartOne() {
         return new Puzzle(new InputReader(INPUT));
     }
 
     @Override
-    protected Integer getExpectedResultPartOne() {
-        return -1;
+    protected Long getExpectedResultPartOne() {
+        return 357L;
     }
 
     @Override
-    protected Solvable<Integer, Integer> getSutPartTwo() {
+    protected Solvable<Long, Long> getSutPartTwo() {
         return new Puzzle(new InputReader(INPUT));
     }
 
     @Override
-    protected Integer getExpectedResultPartTwo() {
-        return -1;
+    protected Long getExpectedResultPartTwo() {
+        return 3121910778619L;
     }
 }
